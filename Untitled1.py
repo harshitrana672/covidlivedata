937e06608f100bfecf589b80aba21d8a4a8b62c8
Harshit Rana <77775131+harshitsak786@users.noreply.github.com>
2021-02-04 13:33:36 +0530

    Add files via upload
    this file brings you to get the data about current status of covid_19

---
+++ Untitled1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from covid import Covid
+
+
+# In[3]:
+
+
+instance=Covid()
+
+
+# In[4]:
+
+
+instance.get_data()
+
+
+# In[6]:
+
+
+instance.get_status_by_country_name("India")
+
+
+# In[ ]:
+
+
+
+
